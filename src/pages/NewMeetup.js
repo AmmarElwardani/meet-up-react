4e055dd6b7801f0aac6cbfe968fd9a784e7dcd00
Ammar <ammarelw@gmail.com>
2022-07-22 10:27:18 +0100

    Adding new meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,29 @@
-const NewMeetupPage = () => {
-  return <div>New Meet up page</div>;
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import {useNavigate} from 'react-router-dom';
+const NewMeetupPage = (props) => {
+  
+    const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData){
+
+    fetch('https://react-getting-started-ca136-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then( ()=> {
+      navigate('/');
+    } );
+
+  }
+
+  return (
+    <section>
+      <h1>Add New Meet up</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
